4a302998bd8d74b5551c379c3eaf8fc42432ceac
Bragadeesh001 <bragadeeshs75@gmail.com>
2023-12-19 09:58:25 +0530

    fixed blockers in timesheet

--- employee_information/views.py
+++ employee_information/views.py
@@ -462,8 +462,8 @@ def TimeSheetCreate(request):
     # Get the next Sunday (6 days after the start_date)
     end_date = start_date + relativedelta(days=6)
     # Filter the leave objects
-    leave_date = leave.objects.filter(employee_name=request.user.employee_name, start_date__gte=start_date, end_date__lte=end_date)
-    print("leave_date",leave_date)
+    #leave_date = leave.objects.filter(employee_name=request.user.employee_name, start_date__gte=start_date, end_date__lte=end_date)
+    
 
     current_user = request.user
     context = {
@@ -475,6 +475,7 @@ def TimeSheetCreate(request):
     if request.method == 'POST':
 
         data = json.loads(request.body)
+        leave_date = leave.objects.filter(employee_name=request.user.employee_name, start_date__gte=data.get('start_date'), end_date__lte=data.get('end_date'))
 
         data["username"] = request.user.employee_name
 
@@ -535,21 +536,16 @@ def TimeSheetCreate(request):
 
             # Convert start_date and end_date to datetime objects
             start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+            
             end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
             # import pdb
             # pdb.set_trace()
             try:
             # Filter data based on start_date and end_date
-                print("try in")
-                print("start_date",start_date)
-                print("username", username)
-                data = model.objects.filter(start_date=start_date, username=username, end_date=end_date )
-                print("try out", data)
-                print("got data",data)
-                print("worked")
-                print("monday_value",data[0].monday_value)
-                print("project:",len(data[0].project_name))
-                print("status",data[0].status)
+                print(start_date, username, end_date)
+                data = model.objects.filter(start_date=start_date, username=username )
+                print("1", data)
+                
                 
                 data = {
                     "start_date": data[0].start_date,
@@ -579,7 +575,8 @@ def TimeSheetCreate(request):
 
 
                 }
-                if leave_date[0]:
+                if leave_date:
+                    print("in leave_date")
                     leave_date_list = list(leave_date.values())
                     data["leave_date"] = leave_date_list
                     data["leave_start_date"] = leave_date[0].start_date
@@ -588,15 +585,36 @@ def TimeSheetCreate(request):
                 print("data",data)
             except Exception:
                 try:
-                    leave_date_list = list(leave_date.values())
-                    data = {
-                        "leave_start_date": leave_date[0].start_date,
-                        "leave_end_date": leave_date[0].end_date,
-                        "leave_status": leave_date[0].status,
-                        "leave_date" : leave_date_list,
-                    }
+                    from datetime import datetime, timedelta
+                    print('in')
+                    
+                    # Get the current date
+                    current_date = datetime.now()
+
+                    # Calculate the most recent past Monday
+                    start_of_week = current_date - timedelta(days=current_date.weekday())
+                    print('454', start_of_week.date(), start_date)
+                    if start_date == start_of_week.date() :
+                        print("in start_date") 
+                        
+                        leave_date_list = list(leave_date.values())
+                        data = {
+                            "leave_start_date": leave_date[0].start_date,
+                            "leave_end_date": leave_date[0].end_date,
+                            "leave_status": leave_date[0].status,
+                            "leave_date" : leave_date_list,
+                        }
+                    else:
+                        print("inside ifffffffffffffffffffffffffffffffff")
+                        if data[0]:
+                            print("daaaaaaaaaaaaaaa", data)
+                            pass
+                        else:
+                            print("eeeeeeeeeeeeeeeeeeeellllllllllse")
+                            data = None
                 except:
                     print("failed with leave date")
+                    data = None 
                 print("111111111111111111111111111111111111111111111111111")
                 # For testing
             # data =            {
