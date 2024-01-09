e13a39b12c304c9013d37179ec389c163656d720
manoj_r <666rmanoj@gmail.com>
2024-01-09 21:48:32 +0530

    Shift in signup form

--- User/forms.py
+++ User/forms.py
@@ -6,4 +6,4 @@ from .models import Employee
 class EmployeeSignUpForm(forms.ModelForm):
     class Meta:
         model = Employee
-        fields = ['employee_id','employee_name','email_id','phonenumber','password']
+        fields = ['employee_id','employee_name','email_id','phonenumber','shift','password']
